7472af06b1190109d14d5ae24a1eb674bfd553a8
Joseph Ayman <josephayman@mail.com>
2023-10-17 10:31:44 +0200

    add print  and indexing operator tests

--- test_array_list.cpp
+++ test_array_list.cpp
@@ -15,7 +15,45 @@ void test_empty_array_has_length_zero()
     std::cout << " - Success!\n";
 }
 
+void test_array_with_two_elements_appended_has_length_two()
+{
+    ArrayList a{};
+    a.append(1);
+    a.append(2);
+    std::cout << "Test that array with two elements appended has length two";
+    assert(a.length() == 2);
+    std::cout << " - Success!\n";
+}
+
+void test_print()
+{
+    ArrayList a{};
+    a.append(1);
+    a.append(2);
+    a.append(3);
+    std::cout << "Test print: ";
+    a.print();
+}
+
+void test_indexing_operator()
+{
+    ArrayList a{};
+    a.append(1);
+    a.append(2);
+    a.append(3);
+    std::cout << "Test indexing operator: ";
+    assert(a[0] == 1);
+    assert(a[1] == 2);
+    assert(a[2] == 3);
+    a[1] = 7;
+    assert(a[1] == 7);
+    std::cout << " - Success!\n";
+}
+
 int main()
 {
     test_empty_array_has_length_zero();
+    test_array_with_two_elements_appended_has_length_two();
+    test_print();
+    test_indexing_operator();
 }
